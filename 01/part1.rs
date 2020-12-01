b4d43e20604659959da3da8071c3b97f9ea635c4
Andy Soffer <asoffer@google.com>
2020-12-01 10:30:56 -0500

    Day 1 Part 1

---
+++ 01/part1.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let contents: String = std::fs::read_to_string("input.txt")
+            .expect("failed to read file");
+    let values: Vec<i32> = contents
+            .split("\n")
+            .map(|s| s.parse::<i32>().unwrap_or(0))
+            .filter(|n| n.is_positive())
+            .collect();
+    for i in 0 .. values.len() {
+      for j in i+1 .. values.len() {
+          if values[i] + values[j] == 2020 {
+            print!("{}\n", values[i] * values[j]);
+          }
+      }
+    }
+}
